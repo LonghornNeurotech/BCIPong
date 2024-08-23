fixed continuous variance calculation

preprocessing/preprocess.py
@@ -15,10 +15,10 @@ class PreProcess:
         self.low = lowcut / self.nyquist
         self.high = highcut / self.nyquist
         self.b, self.a = butter(order, [self.low, self.high], btype='band')
-        self.buffer = np.zeros((16, 192))
-        self.n = np.zeros((16, 1))
+        self.buffer = np.zeros((16, 5000))
         self.mean = np.zeros((16, 1))
-        self.M2 = np.zeros((16, 1))
+        self.var = np.zeros((16, 1))
+        self.n = 0
 
     def update_batch_stats(self, data):
         """
@@ -26,17 +26,18 @@ class PreProcess:
 
         :param data: np.array, shape=(16, *), the data to update the stats with.
         """
+        self.n += data.shape[1]
+        if self.n > 5000:
+            self.mean = np.mean(self.buffer, axis=1, keepdims=True)
+            self.var = np.var(self.buffer, axis=1, keepdims=True)
+            self.std = np.sqrt(self.var)
+            self.n = 5000
 
-        batch_mean = np.mean(data, axis=1, keepdims=True)
-        delta = batch_mean - self.mean
-        batch_M2 = np.sum((data - batch_mean) ** 2, axis=1, keepdims=True)
-        n_new = self.n + data.shape[1]
-
-        self.mean += delta * data.shape[1] / n_new
-        self.M2 += batch_M2 + delta ** 2 * self.n * data.shape[1] / n_new
-        self.n = n_new
-
-        self.buffer = np.concatenate((self.buffer[:, data.shape[1]:], data), axis=1)
+        else:
+            self.mean = np.mean(self.buffer[:, -self.n:], axis=1, keepdims=True)
+            self.var = np.var(self.buffer[:, -self.n:], axis=1, keepdims=True)
+            self.std = np.sqrt(self.var)
+        
 
     def butter_bandpass_filter(self, data):
         """
@@ -56,7 +57,7 @@ class PreProcess:
         :return: np.array, shape=(16, 192), the normalized data
         """
 
-        return (data - self.mean) / np.sqrt(self.M2 / self.n)
+        return (data - self.mean) / self.std
 
     def preprocess(self, data):
         """
@@ -65,7 +66,7 @@ class PreProcess:
         :param data: np.array, shape=(16, *), the data to preprocess
         :return: np.array, shape=(16, 192), the preprocessed data
         """
-
+        self.buffer = np.concatenate((self.buffer[:, data.shape[1]:], data), axis=1)
         self.update_batch_stats(data)
         data = self.buffer
         data = self.butter_bandpass_filter(data)